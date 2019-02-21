f3f7cd27cba046a4c0d1ee83dd1e4734fd989a00
CFlag <cliu0507@users.noreply.github.com>
2019-02-21 13:37:42 -0800

    Create RallyHealth_phone.py

---
+++ myQuestions/RallyHealth_phone.py
@@ -0,0 +1,15 @@
+给一个多叉树
+input是rootnode和一个target node。要求返回从rootnode到targe node的path，并且print out出来
+自己定义treenode的data structure if necessary
+
+                A
+              / | \    
+             B  C  D 
+            |\   |  | 
+           E  F  G  H 
+
+
+
+
+def findPath(rootnode, target):
+     #code here
