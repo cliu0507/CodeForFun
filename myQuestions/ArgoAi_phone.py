e3d621014618b24f629789c0f506a50a568de804
CFlag <cliu0507@users.noreply.github.com>
2019-02-21 13:17:30 -0800

    Create ArgoAi_phone.py

---
+++ myQuestions/ArgoAi_phone.py
@@ -0,0 +1,14 @@
+Questions 1:
+
+input list with unsorted integers:
+input : [1,0,0,4,3,5,0]
+output : [0,0,0,1,4,3,5]
+write an algorithm to push zero element to the head of array but keep the relative order of non-zeros element
+Requirement: no extra space used,
+
+
+Question 2:
+Leetcode 42: Trapping the water (hard)
+
+
+
