ec29e24d9785cec770a53211f41592923461bec2
CFlag <cliu0507@users.noreply.github.com>
2019-02-21 13:50:38 -0800

    Create NuroAi_phone.py

---
+++ myQuestions/NuroAi_phone.py
@@ -0,0 +1,7 @@
+Nuro Ai ML phone interview
+
+1. Discuss a little bit about NN usage in your project experience
+2. What is the biggest problem in RNN? How to solve?
+3. What is the embedding layer?Why needs this
+3. Introduce GAN
+4. What is the general steps of training a GAN?
