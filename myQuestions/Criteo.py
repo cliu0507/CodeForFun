dd2889b9fc67b6d9a57aca94ae844837efbf85f3
CFlag <cliu0507@users.noreply.github.com>
2019-02-21 13:44:35 -0800

    Create Criteo.py

---
+++ myQuestions/Criteo.py
@@ -0,0 +1,21 @@
+Question1:
+
+Give two sorted integer arrays, find the intersection of them:
+
+
+Follow Up 1:
+What if there are unsorted integer arrays
+
+Follow Up 2:
+What if there are unsorted integer arrays and get O(n) solution?
+
+
+Follow Up 3:
+如果两个数组都很大（unsorted），没办法装进一台机子的内存，现在你有100台机子，该怎么优化？
+
+
+
+Question2:
+HashTable low level implementation details:
+
+Answer: array + linkedList(to solve collision)
