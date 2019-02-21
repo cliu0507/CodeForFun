268e2d94df222cc511308a6ded1588a00b490bfd
CFlag <cliu0507@users.noreply.github.com>
2019-02-21 14:03:29 -0800

    Create Aquifi_phone.py

---
+++ myQuestions/Aquifi_phone.py
@@ -0,0 +1,14 @@
+1. Introduct logistic regression to a person who is not familiar with machine learning
+2. Why don't use linear regression to do classification ? if yes, what is the problem. How can logistic regression solve this?
+3. Can cost function in logistic regression can be MSE?  My answer is no, non convex
+4. Any other interpretation of logistic regression cost function? My answer is max entropy theorm.(However, intervewer doesn't know it...)
+   The answer he expects is maximum likehood estimation (damn, i forgot it)
+5. image segmentation algorithm? I answered RCNN
+6. any classic image segmentation algorithm? I answered watershed
+7. He describes one proposal: use kmeans to do image segmentation. basically cluster RGB color (3 features). will it work? i said no, it 
+  will lose spatial information
+8. General ML:
+  a. what is variance and bias 
+  b. knn is high variance or high bias?
+  c. logistic regression is high variance or high bias if the data is real world data?
+  d. what is parameter is knn? I said no parameter, no training, he agrees, he said we can also interpret it as each sample is parameter. (想想其实也对，这样就说明了high variance)
