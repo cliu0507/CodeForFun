daada09c5912936d5fa523b346a0cb625bd1cd6e
CFlag <cliu0507@users.noreply.github.com>
2019-02-21 13:51:24 -0800

    Update Iterable_phone2.py

--- myQuestions/Iterable_phone2.py
+++ myQuestions/Iterable_phone2.py
@@ -1,4 +1,5 @@
-1. What is neural network?
-2. How neural network get trained?
-3. What is back probagation?
-4. What if we can't fit all sample into memory for one iteration?
+1. What is neural network ?
+2. How neural network get trained ?
+3. What is back probagation ?
+4. Have your heard of gradient vanishing and what is it
+5. What if we cannt fit all sample into memory for one iteration?
